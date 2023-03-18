2d3612275447d70a1bdb8a86eca01a2cbd37888d
Olivier Cervara <ocervara@gmail.com>
2023-03-18 11:50:54 +0100

    debug, hashing password ok

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -1,13 +1,21 @@
 const { User } = require("../mongo")
+const bcrypt = require("bcrypt")
 
-function createUser(req, res) {
+async function createUser(req, res) {
     const { email, password } = req.body
 
-    const user = new User({ email, password })
+    const hashedPassword = await hashPassword(password)
+
+    const user = new User({ email, password: hashedPassword })
     
     user.save()
         .then(() => res.status(201).send({ message: "Utilisateur enregistré avec succès!" }))
-        .catch((err) => res.status(500).send({ message: "Erreur lors de l'enregistrement de l'utilisateur." }));
+        .catch((err) => res.status(500).send({ message: "Erreur lors de l'enregistrement de l'utilisateur.", err }));
+}
+
+function hashPassword(password) {
+    const saltRounds = 10
+    return bcrypt.hash(password, saltRounds)
 }
 
 module.exports = { createUser }
