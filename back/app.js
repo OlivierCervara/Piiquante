b4c507d595de6636723cc87264c4f2f0de1b9fbf
Olivier Cervara <ocervara@gmail.com>
2023-03-22 14:23:55 +0100

    refactoring

--- back/app.js
+++ back/app.js
@@ -1,26 +1,6 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
-const cors = require("cors")
+const { app, express } = require("./server")
 const port = 3000
-const multer = require("multer")
 const path = require("path")
-
-const storage = multer.diskStorage({
-    destination: "images/",
-    filename: function (req, file, cb) {
-        cb(null, makeFilename(req, file))
-    }
-})
-
-function makeFilename(req, file){
-    console.log("req, file: ", req, file)
-    const fileName = `${Date.now()}-${file.originalname}`.replace(/\s/g, "-" )
-    file.fileName = fileName
-    return fileName
-}
-
-const upload = multer({ storage: storage })
 
 // Connection to Database
 require("./mongo")
@@ -30,9 +10,7 @@ const { createUser, logUser } = require("./controllers/user")
 const { getSauces, createSauce } = require("./controllers/sauces")
 
 // Middlewares
-app.use(cors())
-app.use(express.json())
-
+const { upload } = require("./middleware/multer")
 const { authenticateUser } = require("./middleware/auth")
 
 // Routes
